6d748e12c78802695a474e604e6c77204084041e
Maksudul Haque <e01716@gmail.com>
2023-09-06 03:16:55 +0600

    added ligue1 module

--- src/Components/Ligue1/LigueOneModule.jsx
+++ src/Components/Ligue1/LigueOneModule.jsx
@@ -15,8 +15,8 @@ const LigueOneModule = () => {
     const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
     const day = currentDate.getDate().toString().padStart(2, "0");
 
-    // const formattedDate = `${year}-${month}-${day}`;
-    const formattedDate = "2023-09-03";
+    const formattedDate = `${year}-${month}-${day}`;
+    // const formattedDate = "2023-09-03";
 
     return formattedDate;
   };
